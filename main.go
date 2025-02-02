b2260106e7e0a2cba3820a9a6edb7d79e2d88bd5
Aleksandr-Osipenko <aleksandr@osipenko.net>
2025-02-03 00:32:52 +0300

    добавление определения порта из переменной окружения

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"net/http"
-	
+	"os"
 
 	"github.com/go-chi/chi/v5"
 )
 
 const (
-	webDir = "./web"
-//	port   = "7540"
+	webDir = "./web" //дирекрория веб-приложения
 )
 
 func main() {
+   //определение порта для запуска приложения
+   port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
+	}
 
+   //
 	r := chi.NewRouter()
    r.Handle("/*", http.FileServer(http.Dir(webDir)))
 	//r.Get("/js/scripts.min.js", handleScripts)
 	
 
-	err := http.ListenAndServe(":7540", r)
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		panic(err)
 	}
