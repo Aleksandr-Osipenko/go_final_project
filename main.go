b6dedabe380eb1d0d07375219f9bfb1d73ffe17a
Aleksandr-Osipenko <aleksandr@osipenko.net>
2025-02-10 05:41:33 +0300

    добавление инициализации базы данных

--- main.go
+++ main.go
@@ -1,17 +1,71 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	_ "modernc.org/sqlite"
 )
 
 const (
-	webDir = "./web" //дирекрория веб-приложения
+	webDir = "./web" //директория веб-приложения
 )
 
+
+
+
 func main() {
+
+   //получение пути до исполняемого файла приложения
+   appPath, err := os.Executable()
+   if err != nil {
+       log.Fatal(err)
+   }
+   //определение наличия базы данных
+   dbFile := filepath.Join(filepath.Dir(appPath), "scheduler.db")
+   _, err = os.Stat(dbFile)
+   
+   if err != nil {
+      log.Println("База данных не обнаружена. Создание новой.")
+   file, err := os.Create("scheduler.db")
+if err != nil {
+    log.Fatal(err)
+    return
+}
+log.Println("Новый файл базы успешно создан")
+file.Close()
+
+db, err := sql.Open("sqlite", "scheduler.db")
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    defer db.Close()
+
+table := `CREATE TABLE scheduler (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+     date CHAR(8) NOT NULL DEFAULT "",
+     title VARCHAR(128) NOT NULL DEFAULT "",
+     comment VARCHAR(128) NOT NULL DEFAULT "",
+     repeat VARCHAR(128) NOT NULL DEFAULT "");
+     CREATE INDEX idx_date ON scheduler (date)`
+     _, err = db.Exec(table)
+   if err != nil {
+       log.Fatal(err)
+       return 
+   }
+   log.Println("Таблица успешно создана!")  
+}
+   
+   
+
+   // если install равен true, после открытия БД требуется выполнить 
+   // sql-запрос с CREATE TABLE и CREATE INDEX 
+
    //определение порта для запуска приложения
    port := os.Getenv("TODO_PORT")
 	if port == "" {
@@ -23,8 +77,8 @@ func main() {
    r.Handle("/*", http.FileServer(http.Dir(webDir)))
 	//r.Get("/js/scripts.min.js", handleScripts)
 	
-
-	err := http.ListenAndServe(":"+port, r)
+   //запуск сервера приложения
+	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		panic(err)
 	}
